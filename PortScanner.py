c405af58ca1a2d43610b45dee5d73bc7c9432482
theKernel <tf875427@gmail.com>
2020-09-20 17:20:26 +0200

    Initial commit Python Scanner

---
+++ PortScanner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+host = input("Enter the IP: ")
+port = int(input("Enter Port: "))
+
+
+def portScanner(port):
+    if s.connect_ex((host, port)):
+        print("This port is closed")
+    else:
+        print("This port is open")
+
+portScanner(port)
